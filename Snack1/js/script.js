578b34b0c840d7432781c10dcaf4d239e9408f4a
javoxpl <liacovone7@outlook.it>
2022-05-24 13:45:44 +0200

    Snack1 completed

--- Snack1/js/script.js
+++ Snack1/js/script.js
@@ -1,1 +1,13 @@
 /*Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.*/
+
+let number = parseInt(prompt("inserisci un numero"));
+
+let div = 0;
+
+
+if (number%2 === div) {
+    console.log(number);
+} else {
+    number = number + 1
+    console.log(number)
+}
