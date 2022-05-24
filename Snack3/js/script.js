c50d4e0f0b372b6c39ca9b83faed1286a472fecc
javoxpl <liacovone7@outlook.it>
2022-05-24 17:07:35 +0200

    Snack3 completed (console)

--- Snack3/js/script.js
+++ Snack3/js/script.js
@@ -1,1 +1,16 @@
 /* Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione (indice) dispari*/
+
+
+let numeri =[]; 
+let rNumeri;
+for (let i = 0; i <= 20; i++) {
+    rNumeri = Math.round(Math.random() * 100);
+    numeri.push(rNumeri);
+    let index = (numeri.indexOf(rNumeri));
+    let indexx = index%2
+    if (indexx != 0) {
+        console.log (rNumeri);
+    }
+
+}
+//console.log(numeri);
